f90973ea85379baadeec351e6593f9d4a658b525
karripartanen <karripartanen25@gmail.com>
2023-10-29 17:19:28 +0200

    tehtävät

--- Moduuli 9/Tehtävä 4.py
+++ Moduuli 9/Tehtävä 4.py
@@ -11,6 +11,8 @@
 # Tämä tehdään kutsumalla kulje-metodia.
 # Kilpailu jatkuu, kunnes jokin autoista on edennyt vähintään 10000 kilometriä.
 # Lopuksi tulostetaan kunkin auton kaikki ominaisuudet selkeäksi taulukoksi muotoiltuna.
+import random
+
 
 class Car:
     def __init__(self, plate, top_spd, cur_spd=0, dist=0):
@@ -20,17 +22,24 @@ class Car:
         self.dist = dist
 
     def values(self):  # auton arvot
-        print(f"Plate: {self.plate}, top speed: {self.top_spd} km/h, "
+        print(f"Plate: {self.plate}, top speed: {self.top_spd}km/h, "
               f"current speed: {self.cur_spd} km/h, distance covered: {self.dist} km.\n")
 
     def accelerate(self, spd_change):  # kiihdytä-metodi
-        if spd_change > 0 and self.cur_spd < self.top_spd:
+        if spd_change < 0:
             self.cur_spd = self.cur_spd + spd_change
-            print(f"The car has accelerated to {self.cur_spd}km/h.\n")
-
-
-
-
+            if self.cur_spd < 0:
+                self.cur_spd = 0
+                print(f"{self.plate} emergency braked and has stopped.\n")
+            elif self.cur_spd > 0:
+                print(f"{self.plate} has slowed down to {self.cur_spd}km/h.\n")
+        elif spd_change > 0:
+            self.cur_spd = self.cur_spd + spd_change
+            if self.cur_spd > self.top_spd:
+                self.cur_spd = self.top_spd
+                print(f"{self.plate} has reached the top speed of: {self.top_spd}km/h\n")
+            elif self.cur_spd < self.top_spd:
+                print(f"{self.plate} has accelerated to {self.cur_spd}km/h\n")
 
     def proceed(self, hour):  # kulje-metodi
         extra_dist = hour * self.cur_spd
@@ -39,6 +48,7 @@ class Car:
         print(f"Distance covered after the drive: {self.dist}")
 
 
+'''
 car1 = Car("ABC-123", 142, 60, 3500)  # nopeudet km/h muodossa
 car1.values()
 
@@ -46,3 +56,27 @@ for i in [30, 70, 50, -200]:
     car1.accelerate(i)
 
 car1.proceed(1.5)
+'''
+race_cars = []
+for i in range(10):
+    plate = (f"ABC-{i + 1}")
+    top_speed = random.randint(100, 200)
+    race_cars.append(Car(plate, top_speed, 0, 0))
+
+race_winner = None
+target = 10000
+while not race_winner:
+    for car in race_cars:
+        car.accelerate(random.randint(-10, 15))
+        car.proceed(1)
+        if car.dist >= target:
+            race_winner = car
+            break
+
+print(f"Winner of the race: {race_winner.plate}!\n")
+print("Results:\n")
+for car in race_cars:
+    print(f"Plate: {car.plate}, top speed: {car.top_spd}km/h, distance driven: {car.dist}km.")
+
+#  aivan kauheaa koodia
+
