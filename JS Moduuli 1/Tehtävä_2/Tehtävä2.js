f3691bd3b91ccb06036d373c4abf7f71d6054fee
karripartanen <karripar@metropolia.fi>
2023-11-19 14:46:13 +0200

    Commit 19.11.2023

--- JS Moduuli 1/Tehtävä_2/Tehtävä2.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-//  haetaan html-elementti
-let fillName = document.querySelector('#fillName');
-
-//  pyydetään nimi
-let name = prompt("What's your name?");
-
-//  printataan käyttäjän nimi
-fillName.innerHTML = `Hello, ${name}!`;
