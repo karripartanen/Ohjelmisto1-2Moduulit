ba9778242d68fb1cb2bd44dd7e31f5abe710f498
karripartanen <karripar@metropolia.fi>
2023-11-26 15:23:40 +0200

    26.11.2023 upload

--- JS_Moduuli3/Module3_assignments/t2/2.js
+++ JS_Moduuli3/Module3_assignments/t2/2.js
@@ -1,10 +1,10 @@
 'use strict'
 const targetElem = document.querySelector('#target');
-const items = ['First item', 'Second item', 'Third item'];  // array of products
+const items = ['First item', 'Second item', 'Third item'];  // array of items
 for (let item of items) {
   let listItem = document.createElement('li');
   let txtNode = document.createTextNode(item);
-  listProd.appendChild(txtNode);
+  listItem.appendChild(txtNode);
   targetElem.appendChild(listItem);
 }
 const second = targetElem.getElementsByTagName('li')[1]; // adding class 'my-item' to second list item
